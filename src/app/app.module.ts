471debf6b8d88433f55cc96c69f64d537adc1975
glapoint22 <glapoint22@gmail.com>
2017-07-17 23:06:24 -0400

    Set home and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  animations: [
+    trigger('menuState', [
+      state('inactive', style({
+            opacity: 0,
+            paddingTop: '0'
+      })),
+      state('active',   style({
+        opacity: 1,
+        paddingTop: '4px'
+      })),
+      transition('inactive <=> active', animate('0.2s ease-in-out'))
+    ])
+  ]
+})
+export class NavigationComponent {
+  isVisible: boolean = false;
+  state: string = 'inactive';
+
+  //Show or hide the mobile menu
+  menuClick(): void {
+        this.isVisible = !this.isVisible;
+        this.state = this.isVisible ? this.state = 'active' : this.state = 'inactive';
+    }
+}
